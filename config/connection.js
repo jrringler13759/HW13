3dd1b112506d5b7471e9b410069ffc540d5d8b58
Nikita <jrringler13759@gmail.com>
2020-06-08 11:40:21 -0400

    redid connection

--- config/connection.js
+++ config/connection.js
@@ -5,22 +5,20 @@ var connection;
 if(process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-  console.log("no connection");
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "bootcamp2020",
+    database: "burgers_db"
+  });
   
 };
-
-
-// });
+ 
 
 
 
 
-// connection = mysql.createConnection({
-//   host: "localhost",
-//   port: 3306,
-//   user: "root",
-//   password: "bootcamp2020",
-  // database: "burgers_db"
 
 // Make connection.
 connection.connect(function(err) {
